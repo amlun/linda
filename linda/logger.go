0b87203cfc7e1cf5894ca41e584562c175023e58
lunweiwei <lunweiwei@gmail.com>
2017-05-17 14:21:48 +0800

    Logger

--- linda/logger.go
+++ linda/logger.go
@@ -2,13 +2,6 @@ package linda
 
 import (
 	log "github.com/sirupsen/logrus"
-	"os"
 )
 
 var Logger = log.New()
-
-func init() {
-	log.SetLevel(log.DebugLevel)
-	Logger.Out = os.Stdout
-	Logger.Debug("init Logger")
-}
