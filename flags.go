77b9bfeb948d9dd3cf6abf8a823a9908dcd31933
lunweiwei <lunweiwei@gmail.com>
2017-05-24 18:37:36 +0800

    modify flag queues to queue add default value of queue `default`

--- flags.go
+++ flags.go
@@ -6,7 +6,7 @@ import (
 )
 
 func init() {
-	flag.StringVar(&settings.Queue, "queues", "", "queue name")
+	flag.StringVar(&settings.Queue, "queue", "default", "queue name")
 	flag.Float64Var(&settings.IntervalFloat, "interval", 5.0, "sleep interval when no jobs are found")
 	flag.IntVar(&settings.Concurrency, "concurrency", 2, "the maximum number of concurrently workers")
 	flag.StringVar(&settings.Connection, "connection", "redis://localhost:6379/", "the url of the broker connection")
