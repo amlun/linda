ea3765456a3028bdca002115f15bd3fe2afdf7bb
lunweiwei <lunweiwei@gmail.com>
2017-05-24 18:38:53 +0800

    add default timeout value

--- flags.go
+++ flags.go
@@ -9,6 +9,7 @@ func init() {
 	flag.StringVar(&settings.Queue, "queue", "default", "queue name")
 	flag.Float64Var(&settings.IntervalFloat, "interval", 5.0, "sleep interval when no jobs are found")
 	flag.IntVar(&settings.Concurrency, "concurrency", 2, "the maximum number of concurrently workers")
+	flag.Int64Var(&settings.Timeout, "timeout", 60, "the reserved job life time")
 	flag.StringVar(&settings.Connection, "connection", "redis://localhost:6379/", "the url of the broker connection")
 }
 
