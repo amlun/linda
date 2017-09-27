ad1689106b1c07dc99a88054ffc30347623de92c
lunweiwei <lunweiwei@gmail.com>
2017-09-27 23:07:10 +0800

    modify doc

--- doc.go
+++ doc.go
@@ -9,38 +9,72 @@ Use ReleaseWithDelay func, you can implement a cron job service.
 
 Simple Usage:
 
-	 package main
+		package main
 
-	 import (
-	      "fmt"
-	      "github.com/amlun/linda"
-	 )
+		import (
+			"fmt"
+			"github.com/amlun/linda"
+			"github.com/sirupsen/logrus"
+			"os"
+			"os/signal"
+			"syscall"
+			"time"
+		)
 
-	 func init() {
-		settings := linda.Settings{
-			Queue:         "scheduler",
-			Connection:      "redis://localhost:6379/",
-			Timeout:       60,
-			IntervalFloat: 5.0,
-			Concurrency:   1,
+		func init() {
+			linda.RegisterWorkers("printArgs", PrintArgs)
 		}
-		linda.SetSettings(settings)
-		linda.RegisterWorkers("DispatcherSeed", DispatcherSeed)
-	 }
 
-	 func main() {
-		if err := linda.Run(); err != nil {
-			fmt.Println("Error:", err)
+		func main() {
+			logrus.SetLevel(logrus.DebugLevel)
+			// broker
+			b, _ := linda.NewBroker("redis://localhost:6379/")
+			// saver
+			s, _ := linda.NewSaver("redis://localhost:6379/")
+			// config
+			c := linda.Config{
+				Queue:     "test",
+				Timeout:   60,
+				Interval:  time.Second,
+				WorkerNum: 4,
+			}
+			quit := signals()
+			linda.Init(c, b, s)
+			go func() {
+				defer func() {
+					linda.Quit()
+				}()
+				<-quit
+			}()
+
+			if err := linda.Run(); err != nil {
+				fmt.Println("Error:", err)
+			}
 		}
-	 }
 
-	 func DispatcherSeed(job *linda.Job) error {
-		broker := linda.GetBroker()
-		// get seed info
-		// do seed job
-		// release job with delay (like a cron job)
-		broker.ReleaseWithDelay("scheduler", job, 60)
-		return nil
-	 }
+		func PrintArgs(args ...interface{}) error {
+			fmt.Println(args)
+			return nil
+		}
+
+		// Signal Handling
+		func signals() <-chan bool {
+			quit := make(chan bool)
+			go func() {
+				signals := make(chan os.Signal)
+				defer close(signals)
+				signal.Notify(signals, syscall.SIGQUIT, syscall.SIGTERM, os.Interrupt)
+				defer signalStop(signals)
+				<-signals
+				quit <- true
+			}()
+			return quit
+		}
+
+		// Stops signals channel.
+		func signalStop(c chan<- os.Signal) {
+			signal.Stop(c)
+		}
+
 */
 package linda
